7b009460ebea81296855fb404ee1ad8fe1816c1b
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2019-02-12 17:16:31 -0200

    Update ScriptPython.py

--- ScriptPython.py
+++ ScriptPython.py
@@ -1,9 +1,10 @@
-import csv, os, datetime, ntpath
+import csv, os, datetime, ntpath, sys
 
 report_file_name = "C:\\ItauPython\\teste.csv"
 report_delimiter = ';'
 output_path = '.'
 splited_files = []
+splited_files_size = 1073741824
 
 def main():
     file_size = getsize(report_file_name)
@@ -11,14 +12,13 @@ def main():
     if file_size > 50:
         splitfile(report_file_name)
         for file in splited_files:
-            formatfile(file)
+            formatfile(file, str(sys.argv[1]))
     else:
-        formatfile(report_file_name)
+        formatfile(report_file_name, str(sys.argv[1]))
 
 def getsize(filename):
     file_size = os.path.getsize(filename)
     return (file_size / (1024 ** 3))
-    #return (file_size)
 
 def splitfile(filename):
     with open(os.path.abspath(filename), "r", newline='', encoding="utf8") as originalFile:
@@ -32,7 +32,7 @@ def splitfile(filename):
             current_out_writer.writerow(header)
         output_file_open.close()
         for row in enumerate(reader):
-            if os.path.getsize(output_file) > 1073741824:
+            if os.path.getsize(output_file) > splited_files_size:
                 current_piece += 1
                 output_file = os.path.join(output_path, str(current_piece) + ntpath.basename(filename))
                 splited_files.append(str(current_piece) + ntpath.basename(filename))
@@ -48,23 +48,22 @@ def splitfile(filename):
                 output_file_open.close()
     os.remove(filename)
 
-def formatfile(filename):
+def formatfile(filename, tablename):
     current_piece = 'ft'
-    cabecalho = '0' + str(datetime.datetime.now())
+    cabecalho = tablename + '_' + str(datetime.datetime.now())
     with open(os.path.abspath(filename), "r", newline='', encoding="utf8") as originalFile:
         reader = csv.reader(originalFile, delimiter=report_delimiter)
-        reader_to_list = list(reader)
-        reader_to_list.remove(reader_to_list[0])
-        total_registros = len(reader_to_list)
-        for i, row in enumerate(reader_to_list):
-            reader_to_list[i].insert(0, '1')
-        reader_to_list.insert(0, ['0', cabecalho])
-        reader_to_list.append(['9', str(total_registros)])
+        next(reader, None)
+        tamanho = sum(1 for row in reader)
+        originalFile.seek(0)
+        next(reader, None)
         output_file = os.path.join(output_path, str(current_piece) + ntpath.basename(filename))
         with open(os.path.abspath(output_file), "w", newline='', encoding="utf8") as output_file_open:
             current_out_writer = csv.writer(output_file_open, delimiter=report_delimiter)
-            for row in reader_to_list:
-                current_out_writer.writerow(row)
+            current_out_writer.writerow(['0', cabecalho])
+            for row in reader:
+                current_out_writer.writerow(['1'] + row)
+            current_out_writer.writerow(['9', str(tamanho)])
         output_file_open.close()
     originalFile.close()
     os.remove(filename)
